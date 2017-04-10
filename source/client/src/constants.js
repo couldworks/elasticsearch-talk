8df3547fd9d500a1aa5658e7a7a39637cc2f439f
Douglas José Ramalho Araujo <doug.ramalho@gmail.com>
2017-04-09 22:11:21 -0300

    Creating client structure to consume elasticsearch database

---
+++ source/client/src/constants.js
@@ -0,0 +1,5 @@
+const constants = {
+	ADD_NEWS: "ADD_NEWS"
+}
+
+export default constants
---
+++ source/client/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "./dist/assets"),
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	devServer: {
+		inline: true,
+		contentBase: "./dist",
+		port: 3000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+		        exclude: /node_modules/,
+		        query: {
+		          presets: ['latest', 'stage-0', 'react']
+		        }
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+				loader: 'json-loader'
+			},
+			{
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+            }
+		]
+	},
+    plugins: [
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {discardComments: {removeAll: true}},
+            canPrint: true
+        })
+    ]
+}
