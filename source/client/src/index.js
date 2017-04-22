9fd0515f69f314a7368a51f3eafb2c04015c46e2
Douglas José Ramalho Araujo <doug.ramalho@gmail.com>
2017-04-21 22:07:12 -0300

    Putting first react component

--- source/client/src/index.js
+++ source/client/src/index.js
@@ -1,9 +1,28 @@
 import storeFactory from "./store";
 import { AllNews } from "./actions";
+import initialState from "./initialState";
+import React from "react";
+import { render } from "react-dom"
 
-const store = storeFactory();
+const store = storeFactory(initialState);
+console.log(store.getState());
+//store.dispatch(AllNews());
 
-store.dispatch(AllNews());
+//react-container
+
+var MyComponent = React.createClass({
+	render(){
+		return <div>
+			<h1>Test</h1>
+			<p>One component</p>
+		</div>
+	}
+});
+
+render(<MyComponent />, document.getElementById("react-container"));
+
+
+
 
 
 
